0ae5fffa0531787f9de0c04b8a8a0124b71d9e37
Arunshaik2001 <arunshaik432@gmail.com>
2024-12-10 17:03:01 +0530

    in app notifications playground

--- apps/appServer/src/index.ts
+++ apps/appServer/src/index.ts
@@ -12,7 +12,13 @@ const port = process.env.APP_SERVER_PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.options('*', cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use("/v1/send", sendRouter);
 app.use("/v1/getToken", authRouter);
 
