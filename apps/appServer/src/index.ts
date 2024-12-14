1a150d26b97ce9639b124a593861b25a2dfc28f8
Arunshaik2001 <arunshaik432@gmail.com>
2024-12-14 16:03:01 +0530

    cors origins update

--- apps/appServer/src/index.ts
+++ apps/appServer/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.APP_SERVER_PORT || 3000;
 
 app.use(cors({
-  origin: '*'
+  origin: ['https://inapp-notifications.dev-boi.com/', 'http://localhost:3000']
 }));
 app.use(express.json());
 
