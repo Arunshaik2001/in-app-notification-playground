db19109c3320640ffcccfdaedb8418f1f58cb517
Arunshaik2001 <arunshaik432@gmail.com>
2024-12-14 10:44:35 +0530

    horizontal scroll disabled

--- apps/web/components/notificationInbox/NotificationList.tsx
+++ apps/web/components/notificationInbox/NotificationList.tsx
@@ -57,7 +57,7 @@ const NotificationList: React.FC<NotificationListProps> = ({ notifications }: No
     }
 
     return (
-        <ul className="space-y-4">
+        <ul className="space-y-4 p-2">
             {notifications.map((notification, index) => (
                 <NotificationItem key={index} notification={notification} />
             ))}
